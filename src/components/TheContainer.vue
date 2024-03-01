8c2bb6d1889c135053a781d868cd8664242a5525
RafaPaludo <rafael.ricardopp@gmail.com>
2024-03-01 14:47:13 -0300

    feat(component): create Header and Component

---
+++ src/components/TheContainer.vue
@@ -0,0 +1,25 @@
+<template>
+  <div 
+    class="container"
+    :style=" { backgroundColor: bgColor } "
+  >
+    <slot />
+  </div>
+</template>
+
+<script setup>
+const props = defineProps({
+  bgColor: String,
+  default: '#FFF'
+})
+
+</script>
+
+<style lang="scss">
+.container {
+  & > *{
+    max-width: 144rem;
+    margin: 0 auto;
+  }
+}
+</style>
