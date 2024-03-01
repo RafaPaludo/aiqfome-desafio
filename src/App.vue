350e3ee0dfeaa7b6070f8e7551c216c02a79ac96
RafaPaludo <rafael.ricardopp@gmail.com>
2024-03-01 15:02:05 -0300

    feat(component): add footer

--- src/App.vue
+++ src/App.vue
@@ -2,11 +2,16 @@
   <TheContainer bgColor="#7B1FA2">
     <TheHeader />
   </TheContainer>
+
+  <TheContainer bgColor="#EEF0F5">
+    <TheFooter />
+  </TheContainer>
 </template>
 
 <script setup>
 import TheHeader from '@/components/TheHeader.vue'
 import TheContainer from '@/components/TheContainer.vue';
+import TheFooter from '@/components/TheFooter.vue';
 </script>
 
 <style lang="scss">
