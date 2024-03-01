e551dff7ca326c6d1e840f35bdf22d5ab6e904f5
RafaPaludo <rafael.ricardopp@gmail.com>
2024-03-01 14:46:42 -0300

    feat: add sass variables and configure vite sass

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,18 @@
 <template>
+  <TheContainer bgColor="#7B1FA2">
+    <TheHeader />
+  </TheContainer>
 </template>
 
 <script setup>
+import TheHeader from '@/components/TheHeader.vue'
+import TheContainer from '@/components/TheContainer.vue';
 </script>
 
-<style scoped>
+<style lang="scss">
+@import "@/assets/scss/global.scss";
+
+#app {
+  font-family: $font-default;
+}
 </style>
