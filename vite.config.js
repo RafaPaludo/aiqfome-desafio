735d3e1ed34b506913eeaddbcdb1fa58359feadc
RafaPaludo <rafael.ricardopp@gmail.com>
2024-03-04 01:47:23 -0300

    feat: add docker and docker compose

--- vite.config.js
+++ vite.config.js
@@ -8,6 +8,10 @@ export default defineConfig({
   plugins: [
     vue(),
   ],
+  server: {
+    host: true,
+    port: 5000
+  },
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
